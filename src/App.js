9d2e37e18ea338118f8aebb7287e0a535c5aa68e
zesik1 <overdrivemono@gmail.com>
2022-06-07 17:33:18 +0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
-
+import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
