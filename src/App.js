017abd014bea68cf702e4e6990f272dac9589e3f
zesik1 <overdrivemono@gmail.com>
2022-06-08 13:12:41 +0300

    cart context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useState } from "react";
 import Cart from "./components/Cart/Cart";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
+import CartProvider from "./components/store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +16,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
